250a2e5ef9fddfa21250e8e0add7e0e0832f6b56
Nima Keivan <nimski@gwu.edu>
2014-08-12 13:55:56 -0400

    Style updates for gui_common.h

--- applications/common/gui_common.h
+++ applications/common/gui_common.h
@@ -13,17 +13,17 @@
 #include <sdtrack/semi_dense_tracker.h>
 #include "etc_common.h"
 
-typedef std::shared_ptr<sdtrack::DenseTrack> TrackPtr ;
+typedef std::shared_ptr<sdtrack::DenseTrack> TrackPtr;
 typedef std::vector<std::pair<Eigen::Vector2d, TrackPtr>> TrackCenterMap;
 
-
 struct TrackerHandler : pangolin::Handler3D {
-  TrackerHandler(pangolin::OpenGlRenderState& cam_state,
-          uint32_t image_w, uint32_t image_h,
-          pangolin::AxisDirection enforce_up=pangolin::AxisNone,
-          float trans_scale=0.01f)
-    : pangolin::Handler3D(cam_state, enforce_up, trans_scale),
-  image_width(image_w), image_height(image_h){}
+  TrackerHandler(pangolin::OpenGlRenderState& cam_state, uint32_t image_w,
+                 uint32_t image_h,
+                 pangolin::AxisDirection enforce_up = pangolin::AxisNone,
+                 float trans_scale = 0.01f)
+      : pangolin::Handler3D(cam_state, enforce_up, trans_scale),
+        image_width(image_w),
+        image_height(image_h) {}
 
   void Mouse(pangolin::View& view, pangolin::MouseButton button, int x, int y,
              bool pressed, int button_state) {
@@ -35,29 +35,29 @@ struct TrackerHandler : pangolin::Handler3D {
 
     // Figure out which track we have clicked
     selected_track = nullptr;
-    for (size_t ii = 0; ii < track_centers.size() ; ++ii) {
+    for (size_t ii = 0; ii < track_centers.size(); ++ii) {
       Eigen::Vector2d center = track_centers[ii].first;
       const double dist = sqrt(sdtrack::powi(x_val - center[0], 2) +
-          sdtrack::powi(y_val - center[1], 2));
+                               sdtrack::powi(y_val - center[1], 2));
       if (dist < 4) {
         // Then this is the selected track.
         selected_track = track_centers[ii].second;
         sdtrack::DenseKeypoint& kp = selected_track->ref_keypoint;
         sdtrack::Patch& p = selected_track->ref_keypoint.patch_pyramid[0];
         const double ncc = sdtrack::ScorePatchesNCC(
-              p.values, selected_track->transfer[0].projected_values, 9, 9);
-        std::cerr << "selected kp " << selected_track->id <<
-                     "with response: " << kp.response << " response2 " <<
-                     kp.response2 << " rmse " <<
-                     selected_track->transfer[0].rmse << " ncc: " << ncc <<
-                     " track ncc " << selected_track->transfer[0].ncc <<
-                     " rho " << selected_track->ref_keypoint.rho << std::endl;
+            p.values, selected_track->transfer[0].projected_values, 9, 9);
+        std::cerr << "selected kp " << selected_track->id
+                  << "with response: " << kp.response << " response2 "
+                  << kp.response2 << " rmse "
+                  << selected_track->transfer[0].rmse << " ncc: " << ncc
+                  << " track ncc " << selected_track->transfer[0].ncc << " rho "
+                  << selected_track->ref_keypoint.rho << std::endl;
         break;
       }
     }
 
-    if(!handled) {
-      Handler3D::Mouse(view,button,x_val,y_val,pressed,button_state);
+    if (!handled) {
+      Handler3D::Mouse(view, button, x_val, y_val, pressed, button_state);
     }
   }
 
@@ -69,19 +69,18 @@ struct TrackerHandler : pangolin::Handler3D {
 
 struct TrackerGuiVars {
   pangolin::OpenGlRenderState render_state;
-  TrackerHandler *handler;
+  TrackerHandler* handler;
   int image_width;
   int image_height;
-  SceneGraph::GLSceneGraph  scene_graph;
+  SceneGraph::GLSceneGraph scene_graph;
   SceneGraph::GLDynamicGrid grid;
-  pangolin::View *grid_view;
+  pangolin::View* grid_view;
   std::vector<pangolin::View*> camera_view;
   pangolin::View patch_view;
-  pangolin::OpenGlRenderState  gl_render3d;
+  pangolin::OpenGlRenderState gl_render3d;
   std::unique_ptr<SceneGraph::HandlerSceneGraph> sg_handler_;
   std::vector<std::vector<std::shared_ptr<SceneGraph::ImageView>>> patches;
 };
-
 
 inline Eigen::Vector2d ImageToWindowCoords(int image_width, int image_height,
                                            double x, double y) {
@@ -95,55 +94,52 @@ void DrawTrackPatches(
     std::shared_ptr<sdtrack::DenseTrack>& track,
     std::vector<std::vector<std::shared_ptr<SceneGraph::ImageView>>>& patches) {
   sdtrack::DenseKeypoint& kp = track->ref_keypoint;
-//  for (uint32_t ii = 0; ii < kp.patch_pyramid.size() &&
-//       ii <= 0 ; ++ii) {
-    int ii = 0;
-    const sdtrack::Patch& ref_patch =
-        kp.patch_pyramid[track->transfer[0].level];
-    std::vector<unsigned char> disp_values;
-    std::vector<unsigned char> disp_proj_values;
-    std::vector<unsigned char> res_values;
-    disp_values.reserve(ref_patch.values.size());
-    disp_proj_values.reserve(disp_values.size());
-    res_values.reserve(disp_values.size());
+  //  for (uint32_t ii = 0; ii < kp.patch_pyramid.size() &&
+  //       ii <= 0 ; ++ii) {
+  int ii = 0;
+  const sdtrack::Patch& ref_patch = kp.patch_pyramid[track->transfer[0].level];
+  std::vector<unsigned char> disp_values;
+  std::vector<unsigned char> disp_proj_values;
+  std::vector<unsigned char> res_values;
+  disp_values.reserve(ref_patch.values.size());
+  disp_proj_values.reserve(disp_values.size());
+  res_values.reserve(disp_values.size());
 
-    for (uint32_t jj = 0; jj < ref_patch.values.size() ; ++jj) {
-      disp_values.push_back(ref_patch.values[jj]);
-      disp_proj_values.push_back(track->transfer[0].projected_values[jj]);
-      res_values.push_back(fabs(track->transfer[0].residuals[jj]));
-    }
-    patches[ii][0]->SetImage(&disp_values[0], ref_patch.dim, ref_patch.dim,
-        GL_LUMINANCE8, GL_LUMINANCE);
+  for (uint32_t jj = 0; jj < ref_patch.values.size(); ++jj) {
+    disp_values.push_back(ref_patch.values[jj]);
+    disp_proj_values.push_back(track->transfer[0].projected_values[jj]);
+    res_values.push_back(fabs(track->transfer[0].residuals[jj]));
+  }
+  patches[ii][0]->SetImage(&disp_values[0], ref_patch.dim, ref_patch.dim,
+                           GL_LUMINANCE8, GL_LUMINANCE);
 
-    patches[ii][1]->SetImage(&disp_proj_values[0], ref_patch.dim,
-        ref_patch.dim, GL_LUMINANCE8, GL_LUMINANCE);
+  patches[ii][1]->SetImage(&disp_proj_values[0], ref_patch.dim, ref_patch.dim,
+                           GL_LUMINANCE8, GL_LUMINANCE);
 
-    patches[ii][2]->SetImage(&res_values[0], ref_patch.dim, ref_patch.dim,
-        GL_LUMINANCE8,GL_LUMINANCE);
+  patches[ii][2]->SetImage(&res_values[0], ref_patch.dim, ref_patch.dim,
+                           GL_LUMINANCE8, GL_LUMINANCE);
   // }
 }
 
 void DrawLandmarks(const uint32_t min_lm_measurements_for_drawing,
                    std::vector<std::shared_ptr<sdtrack::TrackerPose>>& poses,
-                   calibu::Rig<Scalar>& rig,
-                   TrackerHandler *handler,
+                   calibu::Rig<Scalar>& rig, TrackerHandler* handler,
                    int& selected_track_id) {
   glBegin(GL_POINTS);
-  for (std::shared_ptr<sdtrack::TrackerPose> pose: poses) {
+  for (std::shared_ptr<sdtrack::TrackerPose> pose : poses) {
     for (std::shared_ptr<sdtrack::DenseTrack> track : pose->tracks) {
       if (selected_track_id == track->id) {
         handler->selected_track = track;
         selected_track_id = -1;
       }
 
-      if (track->num_good_tracked_frames <
-          min_lm_measurements_for_drawing) {
+      if (track->num_good_tracked_frames < min_lm_measurements_for_drawing) {
         continue;
       }
       Eigen::Vector4d ray;
       ray.head<3>() = track->ref_keypoint.ray;
       ray[3] = track->ref_keypoint.rho;
-      ray = sdtrack::MultHomogeneous(pose->t_wp  * rig.t_wc_[0], ray);
+      ray = sdtrack::MultHomogeneous(pose->t_wp * rig.t_wc_[0], ray);
       ray /= ray[3];
       if (handler->selected_track == track) {
         glColor3f(1.0, 1.0, 0.2);
@@ -168,8 +164,8 @@ void DrawTrackData(std::shared_ptr<sdtrack::DenseTrack>& track,
     rgb = Eigen::Vector3d(1.0, 1.0, 0.2);
   } else if (track->is_outlier == false) {
     const double error =
-        (1.0 - (std::max(track->transfer[cam_id].ncc - 0.8, 0.0) / 0.2))
-        * 0.7 + 0.3;
+        (1.0 - (std::max(track->transfer[cam_id].ncc - 0.8, 0.0) / 0.2)) * 0.7 +
+        0.3;
     sdtrack::hsv2rgb(Eigen::Vector3d(1.0 - error, 1.0, 1.0), rgb);
   } else {
     rgb = Eigen::Vector3d(1.0, 0.2, 0.0);
@@ -178,14 +174,15 @@ void DrawTrackData(std::shared_ptr<sdtrack::DenseTrack>& track,
   glColor4d(rgb[0], rgb[1], rgb[2], 1.0);
   glBegin(GL_LINE_STRIP);
   double alpha = track->keypoints.size() == 1 ? 1.0 : 0.15;
-  const double alpha_increment = track->keypoints.size() == 1  ?
-        0 : (1.0 - alpha) / track->keypoints.size();
+  const double alpha_increment = track->keypoints.size() == 1
+                                     ? 0
+                                     : (1.0 - alpha) / track->keypoints.size();
   for (std::vector<sdtrack::Keypoint> keypoints : track->keypoints) {
     const Eigen::Vector2d& point = keypoints[cam_id].kp;
     if (keypoints[cam_id].tracked) {
       glColor4d(rgb[0], rgb[1], rgb[2], alpha);
-      Eigen::Vector2d px_win = ImageToWindowCoords(image_width, image_height,
-                                                   point[0], point[1]);
+      Eigen::Vector2d px_win =
+          ImageToWindowCoords(image_width, image_height, point[0], point[1]);
       glVertex3f(px_win[0], px_win[1], 0);
     }
     alpha += alpha_increment;
@@ -198,22 +195,21 @@ void DrawTrackData(std::shared_ptr<sdtrack::DenseTrack>& track,
 
   glBegin(GL_LINE_STRIP);
   for (Eigen::Vector2d point : perimiter) {
-    Eigen::Vector2d px_win = ImageToWindowCoords(image_width, image_height,
-                                                 point[0], point[1]);
+    Eigen::Vector2d px_win =
+        ImageToWindowCoords(image_width, image_height, point[0], point[1]);
     glVertex3f(px_win[0], px_win[1], 0);
   }
   glEnd();
 
   glColor3f(0.0, 1.0, 0.0);
   glPointSize(2);
-  Eigen::Vector2d px_win = ImageToWindowCoords(image_width, image_height,
-        track->keypoints.back()[cam_id].kp[0],
+  Eigen::Vector2d px_win = ImageToWindowCoords(
+      image_width, image_height, track->keypoints.back()[cam_id].kp[0],
       track->keypoints.back()[cam_id].kp[1]);
   glBegin(GL_POINTS);
   glVertex3f(px_win[0], px_win[1], 0);
   glEnd();
 }
-
 
 // create image views and layout
 void CreatePatchGrid(
@@ -226,42 +222,40 @@ void CreatePatchGrid(
   float xstep_delta = xstep * 0.9;
 
   patches.resize(num_rows);
-  for (unsigned int ii=0; ii < num_rows; ++ii) {
-    float top =  1.0-ii*ystep;
+  for (unsigned int ii = 0; ii < num_rows; ++ii) {
+    float top = 1.0 - ii * ystep;
     float bottom = top - ystep_delta;
     patches[ii].resize(num_cols);
-    for (unsigned int jj=0; jj < num_cols; ++jj) {
-      float left  = jj*xstep;
+    for (unsigned int jj = 0; jj < num_cols; ++jj) {
+      float left = jj * xstep;
       float right = left + xstep_delta;
       patches[ii][jj] =
           std::shared_ptr<SceneGraph::ImageView>(new SceneGraph::ImageView);
       patch_view.AddDisplay(*patches[ii][jj].get());
       patches[ii][jj]->SetSamplingLinear(false);
-      patches[ii][jj]->SetBounds(bottom,top,left,right);
+      patches[ii][jj]->SetBounds(bottom, top, left, right);
     }
   }
 }
 
 void InitTrackerGui(TrackerGuiVars& vars, uint32_t window_width,
                     uint32_t window_height, uint32_t handler_image_width,
-                    uint32_t handler_image_height,
-                    uint32_t num_cameras) {
+                    uint32_t handler_image_height, uint32_t num_cameras) {
   vars.image_height = handler_image_height;
   vars.image_width = handler_image_width;
   pangolin::CreateWindowAndBind("2dtracker", window_width * 2, window_height);
 
-  vars.render_state.SetModelViewMatrix( pangolin::IdentityMatrix() );
-  vars.render_state.SetProjectionMatrix(
-        pangolin::ProjectionMatrixOrthographic(0, window_width, 0,
-                                               window_height, 0, 1000));
+  vars.render_state.SetModelViewMatrix(pangolin::IdentityMatrix());
+  vars.render_state.SetProjectionMatrix(pangolin::ProjectionMatrixOrthographic(
+      0, window_width, 0, window_height, 0, 1000));
   vars.handler = new TrackerHandler(vars.render_state, vars.image_width,
                                     vars.image_height);
 
-  glPixelStorei(GL_PACK_ALIGNMENT,1);
-  glPixelStorei(GL_UNPACK_ALIGNMENT,1);
+  glPixelStorei(GL_PACK_ALIGNMENT, 1);
+  glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-  glEnable( GL_BLEND );
+  glEnable(GL_BLEND);
 
   // vars.grid.SetNumLines(20);
   // vars.grid.SetLineSpacing(5.0);
@@ -270,28 +264,27 @@ void InitTrackerGui(TrackerGuiVars& vars, uint32_t window_width,
   // Add named OpenGL viewport to window and provide 3D Handler
   vars.camera_view.resize(num_cameras);
   for (int cam_id = 0; cam_id < num_cameras; ++cam_id) {
-    vars.camera_view[cam_id] = &pangolin::CreateDisplay()
-        .SetAspect(-(float)window_width/(float)window_height);
+    vars.camera_view[cam_id] =
+        &pangolin::CreateDisplay().SetAspect(-(float)window_width /
+                                             (float)window_height);
   }
   vars.grid_view = &pangolin::Display("grid")
-      .SetAspect(-(float)window_width/(float)window_height);
+                        .SetAspect(-(float)window_width / (float)window_height);
 
   vars.gl_render3d.SetProjectionMatrix(
-        pangolin::ProjectionMatrix(640,480,420,420,320,240,0.01,5000));
+      pangolin::ProjectionMatrix(640, 480, 420, 420, 320, 240, 0.01, 5000));
   vars.gl_render3d.SetModelViewMatrix(
-        pangolin::ModelViewLookAt(-3,-3,-4, 0,0,0, pangolin::AxisNegZ));
-  vars.sg_handler_.reset(
-        new SceneGraph::HandlerSceneGraph(vars.scene_graph, vars.gl_render3d,
-                                          pangolin::AxisNegZ, 50.0f));
+      pangolin::ModelViewLookAt(-3, -3, -4, 0, 0, 0, pangolin::AxisNegZ));
+  vars.sg_handler_.reset(new SceneGraph::HandlerSceneGraph(
+      vars.scene_graph, vars.gl_render3d, pangolin::AxisNegZ, 50.0f));
   vars.grid_view->SetHandler(vars.sg_handler_.get());
-  vars.grid_view->SetDrawFunction(SceneGraph::ActivateDrawFunctor(
-                                    vars.scene_graph, vars.gl_render3d));
+  vars.grid_view->SetDrawFunction(
+      SceneGraph::ActivateDrawFunctor(vars.scene_graph, vars.gl_render3d));
 
   //.SetBounds(0.0, 1.0, 0, 1.0, -(float)window_width/(float)window_height);
 
-  pangolin::Display("multi")
-      .SetBounds(1.0, 0.0, 0.0, 1.0)
-      .SetLayout(pangolin::LayoutEqual);
+  pangolin::Display("multi").SetBounds(1.0, 0.0, 0.0, 1.0).SetLayout(
+      pangolin::LayoutEqual);
 
   for (int cam_id = 0; cam_id < num_cameras; ++cam_id) {
     pangolin::Display("multi").AddDisplay(*vars.camera_view[cam_id]);
@@ -300,19 +293,18 @@ void InitTrackerGui(TrackerGuiVars& vars, uint32_t window_width,
   pangolin::Display("multi").AddDisplay(*vars.grid_view);
 
   SceneGraph::GLSceneGraph::ApplyPreferredGlSettings();
-  glClearColor(0.0,0.0,0.0,1.0);
+  glClearColor(0.0, 0.0, 0.0, 1.0);
 
   // Create the patch grid.
   vars.camera_view[0]->AddDisplay(vars.patch_view);
   vars.camera_view[0]->SetHandler(vars.handler);
-  vars.patch_view.SetBounds(0.01, 0.31, 0.69, .99, 1.0f/1.0f);
+  vars.patch_view.SetBounds(0.01, 0.31, 0.69, .99, 1.0f / 1.0f);
 
-  CreatePatchGrid(3, 3,  vars.patches, vars.patch_view);
+  CreatePatchGrid(3, 3, vars.patches, vars.patch_view);
 }
 
-bool LoadCameraAndRig(GetPot& cl,
-             hal::Camera& camera_device,
-             calibu::CameraRigT<Scalar>& rig) {
+bool LoadCameraAndRig(GetPot& cl, hal::Camera& camera_device,
+                      calibu::CameraRigT<Scalar>& rig) {
   std::string cam_string = cl.follow("", "-cam");
   try {
     camera_device = hal::Camera(hal::Uri(cam_string));
@@ -338,25 +330,24 @@ bool LoadCameraAndRig(GetPot& cl,
 
   calibu::CameraRigT<Scalar> crig;
   crig = calibu::ToCoordinateConvention<Scalar>(
-        xmlrig, calibu::RdfRobotics.cast<Scalar>());
+      xmlrig, calibu::RdfRobotics.cast<Scalar>());
 
   Sophus::SE3t M_rv;
   M_rv.so3() = calibu::RdfRobotics;
   for (calibu::CameraModelAndTransformT<Scalar>& model : crig.cameras) {
-    model.T_wc = model.T_wc*M_rv;
+    model.T_wc = model.T_wc * M_rv;
   }
   LOG(INFO) << "Starting Tvs: " << crig.cameras[0].T_wc.matrix();
 
   rig.cameras.clear();
   for (uint32_t cam_id = 0; cam_id < crig.cameras.size(); ++cam_id) {
-    rig.Add( crig.cameras[cam_id]);
+    rig.Add(crig.cameras[cam_id]);
   }
 
-  for (size_t ii=0; ii < rig.cameras.size(); ++ii) {
-    LOG(INFO) << ">>>>>>>> Camera " << ii << ":"  << std::endl
-              << "Model: " << std::endl
-              << rig.cameras[ii].camera.K() << std::endl
-              << "Pose: " << std::endl
+  for (size_t ii = 0; ii < rig.cameras.size(); ++ii) {
+    LOG(INFO) << ">>>>>>>> Camera " << ii << ":" << std::endl
+              << "Model: " << std::endl << rig.cameras[ii].camera.K()
+              << std::endl << "Pose: " << std::endl
               << rig.cameras[ii].T_wc.matrix();
   }
   return true;
