f7699a5dcdeaea6787b96361614408ca0c38f66b
Fernando Nobre <fernobre@gmail.com>
2015-02-26 13:49:56 -0800

    lowercased Sophus include for case-sensitive linux installs, removed nonfree open cv SURF feature extractor and references (not used)

--- include/sdtrack/semi_dense_tracker.h
+++ include/sdtrack/semi_dense_tracker.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/nonfree/features2d.hpp>
+//#include <opencv2/nonfree/features2d.hpp>
 #include "options.h"
 #include "track.h"
 #include "keypoint.h"
